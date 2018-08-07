0819167821222abd11bd68f0ad3a458a96fef0ba
yuwen <yuwen@yuwens-MBP.gateway>
2018-08-07 14:47:20 +0930

    moudule 11

--- AnuglarTut-Project-master/APM-Start/src/app/app.module.ts
+++ AnuglarTut-Project-master/APM-Start/src/app/app.module.ts
@@ -2,22 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductListComponet } from './products/product-list.components';
+import { ProductListComponent } from './products/product-list.components';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponet,
+    ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent },
+      {path: 'products/:id', component: ProductDetailComponent },
+      {path: 'welcome', component: WelcomeComponent },
+      {path: '', redirectTo: 'welcome', pathMatch: 'full' }
+    ],{ useHash: true})
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
+ 
--- AnuglarTut-Project-master/APM-Start/src/app/products/product-list.components.ts
+++ AnuglarTut-Project-master/APM-Start/src/app/products/product-list.components.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -7,55 +8,25 @@ import { IProduct } from './product';
     styleUrls:['./product-list.component.css']
 })
 
-export class ProductListComponet implements OnInit{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number=50;
     imageMargin: number =2;
     showImage:boolean=false;
     _listFilter:string;
+    errorMessage: string;
+    private _productService;
     get listFilter():string{
         return this._listFilter;
     }
     set listFilter(value:string){
         this._listFilter=value;
         this.filteredProducts=this.listFilter ? this.performFilter(this.listFilter):this.products;
-console.log(this.filteredProducts);
-        
+        console.log(this.filteredProducts);      
     }
     filteredProducts: IProduct[];
-   products: IProduct[]= [{
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-      }];
-      constructor(){
-          this.filteredProducts=this.products;
-          this.listFilter='cart';
+    products: IProduct[]= [];
+      constructor(private productService: ProductService){
       }
       toggleImage():void{
           this.showImage=!this.showImage;
@@ -63,7 +34,15 @@ console.log(this.filteredProducts);
 
       ngOnInit(): void{
           console.log('In OnInit');
-      }
+          this.filteredProducts=this.products;
+          this.productService.getProducts().subscribe(
+              products => {
+                  this.products = products;
+                  this.filteredProducts = this.products;
+                },
+              error => this.errorMessage = <any>error
+          );
+        }
 
       performFilter(filterBy:string):IProduct[]{
           filterBy =filterBy.toLocaleLowerCase();
